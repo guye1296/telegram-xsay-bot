119c65018f88f093ff0e8939a155f1ad0ab6a487
guye1296 <ezer.guy@gmail.com>
2022-06-15 17:12:38 +0300

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+
+setup(
+    name='xsay_telegram_bot',
+    version='1.0.0',
+    description='A telegram bot that sends funny pictures to your friends',
+    author='guye1296',
+    package_dir={"": 'xsay_bot'},
+    packages=find_packages(where='xsay_bot'),
+    python_requires='>=3',
+    install_requires=(here / 'requirements.txt').read_text().splitlines(),
+    package_data={
+        'xsay_telegram_bot': ["resources/*"],
+    },
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
