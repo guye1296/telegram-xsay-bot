29f2438dcf9af626a6cfe9bf825069c82f362dcb
guye1296 <ezer.guy@gmail.com>
2022-06-15 18:56:22 +0300

    add run_xsay_telegram_bot entrypoint

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ setup(
     version='1.0.0',
     description='A telegram bot that sends funny pictures to your friends',
     author='guye1296',
-#    package_dir={"": 'xsay_bot'},
     packages=find_packages(),
-    python_requires='>=3',
+    python_requires='>=3.7',
     install_requires=(here / 'requirements.txt').read_text().splitlines(),
     package_data={
         'xsay_telegram_bot': ["resources/*"],
     },
     entry_points={
         'console_scripts': [
-            'draw_text_bubble=xsay_telegram_bot.xsay:main'
+            'draw_text_bubble=xsay_telegram_bot.xsay:main',
+            'run_xsay_telegram_bot=xsay_telegram_bot.bot_main:main',
         ]
     }
 )
